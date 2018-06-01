e6084d2eb30d8286fb46ba8a124dcbdfecdd53a7
Ben Cavens <cavensben@gmail.com>
2018-06-01 12:09:19 +0200

    Refactor tree factory

--- src/TreeFactory.php
+++ src/TreeFactory.php
@@ -37,63 +37,35 @@ class TreeFactory
 
     public function create(Transposable $transposable)
     {
-        foreach($transposable->all() as $i => $entry)
-        {
-            $id = is_object($entry) ? $entry->{$transposable->key()}: $entry[$transposable->key()];
-            $parentId = is_object($entry) ? $entry->{$transposable->parentKey()}: $entry[$transposable->parentKey()];
+        $this->hydrate($transposable);
 
-            $entryNode = new Node($entry);
+        $this->addOrphans();
 
-            $this->index[$id] = $entryNode;
-            $this->addAsChild($parentId, $entryNode);
-        }
+        $this->identifyRootNodes();
 
-        // All orphans need to be assigned to their respective parents
-        foreach($this->orphans as $parentId => $orphans)
-        {
-            if(!isset($this->index[$parentId]))
-            {
-                // Strict check which means there is a node assigned to an non-existing parent
-                if($this->strict)
-                {
-                    throw new \LogicException('Parent reference to a non-existing node via identifier ['.$parentId.']');
-                }
-
-                continue;
-            }
-
-            $this->index[$parentId]->addChildren($orphans->all());
-        }
-
-        foreach($this->index as $node)
-        {
-            if($node->isRoot())
-            {
-                $this->roots[] = $node;
-            }
-        }
-
-        // At this point we allow to alter each entry.
-        // Useful to add values depending on the node structure
-        if(method_exists($transposable,'entry'))
-        {
-            foreach($this->index as $node)
-            {
-                $node->replaceEntry($transposable->entry($node));
-            }
-        }
-
-        // At this point we will sort all children should the transposer has set a key to sort on
-        if(property_exists($transposable, 'sortChildrenBy'))
-        {
-            foreach($this->index as $node)
-            {
-                $node->sort($transposable->sortChildrenBy);
-            }
-        }
+        $this->structureCollection($transposable);
 
         // Collect all root nodes because they contain the entire tree
         return new Tree($this->roots, $this->index);
+    }
+
+    private function hydrate(Transposable $transposable)
+    {
+        $id_key = $transposable->key();
+        $parent_key = $transposable->ParentKey();
+
+        foreach ($transposable->all() as $i => $entry) {
+            $id = is_object($entry) ? $entry->{$id_key} : $entry[$id_key];
+            $parentId = is_object($entry) ? $entry->{$parent_key} : $entry[$parent_key];
+
+            $entryNode = new Node($entry);
+
+            // Keep track of flattened list of all nodes
+            $this->index[$id] = $entryNode;
+
+            // Add node to tree
+            $this->addChild($parentId, $entryNode);
+        }
     }
 
     /**
@@ -101,7 +73,7 @@ class TreeFactory
      * @param $entryNode
      * @return mixed
      */
-    private function addAsChild($parentId, $entryNode)
+    private function addChild($parentId, $entryNode)
     {
         if (!$parentId) return;
 
@@ -110,18 +82,68 @@ class TreeFactory
             return;
         }
 
-        $this->addOrphan($parentId, $entryNode);
+        $this->catchOrphan($parentId, $entryNode);
     }
 
     /**
      * @param $parentId
      * @param $entryNode
      */
-    private function addOrphan($parentId, $entryNode)
+    private function catchOrphan($parentId, $entryNode)
     {
         if (!isset($this->orphans[$parentId])) {
             $this->orphans[$parentId] = new NodeCollection();
         }
         $this->orphans[$parentId][] = $entryNode;
+    }
+
+    /**
+     * All orphans need to be assigned to their respective parents
+     */
+    private function addOrphans()
+    {
+        foreach ($this->orphans as $parentId => $orphans) {
+            if (!isset($this->index[$parentId])) {
+
+                // Strict check which means there is a node assigned to an non-existing parent
+                if ($this->strict) {
+                    throw new \LogicException('Parent reference to a non-existing node via identifier [' . $parentId . ']');
+                }
+
+                continue;
+            }
+
+            $this->index[$parentId]->addChildren($orphans->all());
+        }
+    }
+
+    private function identifyRootNodes()
+    {
+        foreach ($this->index as $node) {
+            if ($node->isRoot()) {
+                $this->roots[] = $node;
+            }
+        }
+    }
+
+    /**
+     * @param Transposable $transposable
+     */
+    private function structureCollection(Transposable $transposable)
+    {
+// At this point we allow to alter each entry.
+        // Useful to add values depending on the node structure
+        if (method_exists($transposable, 'entry')) {
+            foreach ($this->index as $node) {
+                $node->replaceEntry($transposable->entry($node));
+            }
+        }
+
+        // At this point we will sort all children should the transposer has set a key to sort on
+        if (property_exists($transposable, 'sortChildrenBy')) {
+            foreach ($this->index as $node) {
+                $node->sort($transposable->sortChildrenBy);
+            }
+        }
     }
 }
