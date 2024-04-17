6c86a97aa276381394e5fc2ead975d45a2f27f22
Ben Cavens <cavensben@gmail.com>
2024-04-17 21:39:32 +0200

    Remove unused class

--- src/Debug/ArrayPresenter.php
+++ src/Debug/ArrayPresenter.php
@@ -2,44 +2,17 @@
 
 namespace Thinktomorrow\Vine\Debug;
 
-use Thinktomorrow\Vine\Node;
 use Thinktomorrow\Vine\NodeCollection;
 
-class ArrayPresenter extends BasePresenter
+class ArrayPresenter
 {
     /**
      * Render html filter tree.
      *
      * @return array
      */
-    public function render()
+    public function render(NodeCollection $collection): array
     {
-        return $this->collection->toArray();
-    }
-
-    /**
-     * Render each node and its children recursively.
-     *
-     * Children are added inside a specific 'children' property and are not nested inside the parent element itself.
-     * This allows for a cleaner recursion and faster rendering of the tree.
-     *
-     * @param NodeCollection $nodeCollection
-     * @param int            $level
-     *
-     * @return array
-     */
-    protected function renderRecursiveToArray(NodeCollection $nodeCollection, $level = 0): array
-    {
-        $output = [];
-
-        foreach ($nodeCollection as $node) {
-            $output[] = $node->getNodeEntry(2);
-
-            if ($node->hasChildNodes()) {
-                $output[] = $this->renderRecursiveToArray($node->getChildNodes(), $level + 1);
-            }
-        }
-
-        return $output;
+        return $collection->toArray();
     }
 }
